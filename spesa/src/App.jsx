82e94ab57f0bbbe8ce250274498c90ee687b0ff1
simone-poli <polismn97@gmail.com>
2025-11-05 19:21:36 +0100

    milestone2

--- spesa/src/App.jsx
+++ spesa/src/App.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react"
+
 function App() {
   
   
@@ -8,6 +10,21 @@ function App() {
   { name: 'Pasta', price: 0.7 },
 ];
 
+
+const [addedProducts, setAddedProducts] = useState([])
+
+
+
+
+function addToCard (product) {
+    const control = addedProducts.find(cartProduct => cartProduct.name === product.name)
+    if  (control) {
+     return 
+    } else {
+      setAddedProducts(prev => [...prev, {...product, quantity:1}] )
+    }
+}
+
   return (
     <>
       <h1>Prodotti tra cui scegliere</h1>
@@ -16,11 +33,24 @@ function App() {
         { products.map ((p,i) => (
 
           <li key={i}> 
-            <p>{p.name} prezzo: {p.price.toFixed(2)}€</p>
+            <p>{p.name} di cui Prezzo: {p.price.toFixed(2)}€</p>
+            <button onClick={() => addToCard(p)}>Aggiungi al carrello</button>
           </li>
         ) )
 
         }
+      </ul>
+
+
+      <h1>Carrello</h1>
+      <ul>
+          {addedProducts.map((p,i) => (
+        <li key={i}>
+            <p>Nome Prodotto: {p.name}</p>
+            <p>Prezzo: {p.price.toFixed(2)}€</p>
+            <p>Quantità: {p.quantity}</p>
+        </li>
+          ))}
       </ul>
     </>
   )
